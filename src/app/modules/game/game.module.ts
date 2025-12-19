5b843c1c8d6495c9eb03d86b54e3d263a4f5e46b
Bernardo Sampaio Floriano Machado <bernardosfmachado@hotmail.com>
2025-12-19 18:36:46 -0300

    melhorias na tabela de jogo

--- src/app/modules/game/game.module.ts
+++ src/app/modules/game/game.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +9,7 @@ import { GamePageRoutingModule } from './game-routing.module';
 import { GamePage } from './game.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    GamePageRoutingModule
-  ],
-  declarations: [GamePage]
+  imports: [CommonModule, FormsModule, IonicModule, GamePageRoutingModule, ReactiveFormsModule],
+  declarations: [GamePage],
 })
 export class GamePageModule {}
--- src/app/shared/models/player.model.ts
+++ src/app/shared/models/player.model.ts
@@ -1,15 +1,16 @@
 export interface IPlayer {
+  [key: string]: any;
   name: string;
-  ponto1: number | null;
-  ponto2: number | null;
-  ponto3: number | null;
-  ponto4: number | null;
-  ponto5: number | null;
-  ponto6: number | null;
-  pontoS: number | null;
-  pontoF: number | null;
-  pontoP: number | null;
-  pontoG: number | null;
+  ponto1: number | string | null;
+  ponto2: number | string | null;
+  ponto3: number | string | null;
+  ponto4: number | string | null;
+  ponto5: number | string | null;
+  ponto6: number | string | null;
+  pontoS: number | string | null;
+  pontoF: number | string | null;
+  pontoP: number | string | null;
+  pontoG: number | string | null;
   total: number;
   index: number;
 }
